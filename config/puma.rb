0bf17d2a53d81759724bab6e3650277d64288c60
Quintin Adam <QuintinAdam@live.com>
2015-01-17 10:10:56 -0700

    procfile and puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,11 +1,18 @@
-threads 1,16
-workers 1
+workers Integer(ENV['PUMA_WORKERS'] || 3)
+threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
+
 preload_app!
-pidfile "tmp/pids/puma.pid"
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  require "active_record"
-  cwd = File.dirname(__FILE__)+"/.."
-  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || YAML.load_file("#{cwd}/config/database.yml")[ENV["RAILS_ENV"]])
+  # worker specific setup
+  ActiveSupport.on_load(:active_record) do
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+                Rails.application.config.database_configuration[Rails.env]
+    config['pool'] = ENV['MAX_THREADS'] || 16
+    ActiveRecord::Base.establish_connection(config)
+  end
 end
