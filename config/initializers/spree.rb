4e72ed36950615e7c1f22cea3822660b4c6ebbbf
Quintin Adam <quintinadam@live.com>
2015-06-20 20:53:31 -0600

    cleaning up old code

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -6,10 +6,53 @@
 # In order to initialize a setting do:
 # config.setting_name = 'new value'
 Spree.config do |config|
-  # Example:
-  # Uncomment to stop tracking inventory levels in the application
-  # config.track_inventory_levels = false
+  # stop tracking inventory levels in the application
+  config.track_inventory_levels = false
+  # logo for store
+  # config.logo = "triquest/logo.png"
+  # config.admin_interface_logo = "triquest/logo.png"
+  # config.products_per_page = 20
+  # config.admin_products_per_page = 20
+  # begin
+    # config.default_country_id = Spree::Country.find_by_name('United States').id
+  # rescue
+    # such a good work around =D
+  # end
+  # config.shipping_instructions = true
+  # config.alternative_shipping_phone = true
+  # config.show_descendents = false
+  # config.always_include_confirm_step = true
+  # config.company = true # Request company field for billing and shipping addr
 end
 
 Spree.user_class = "Spree::User"
+
+# attachment_config = {
+
+#   s3_credentials: {
+#     access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+#     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+#     bucket:            ENV['FOG_DIRECTORY']
+#   },
+
+#   storage:        :s3,
+#   s3_headers:     { "Cache-Control" => "max-age=31557600" },
+#   s3_protocol:    "https",
+#   bucket:         ENV['FOG_DIRECTORY'],
+#   url:            ":s3_domain_url",
+
+#   styles: {
+#       mini:     "48x48>",  # thumbs under image
+#       small:    "220x220>", # images on category view
+#       product:  "470x470>", # full product image
+#       large:    "600x600>" # light box image
+#   },
+#   path:           "/store/:class/:id/:style/:basename.:extension",
+#   default_url:    "/store/:class/:id/:style/:basename.:extension",
+#   default_style:  "product"
+# }
+# attachment_config.each do |key, value|
+#   Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+# end
+# Spree::Image.attachment_definitions[:attachment][:convert_options] = {:all=>"-strip -auto-orient"}
 
