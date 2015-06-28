6621432a49a22c5b40f5b7923de1d1a7aa5a7f62
Quintin Adam <quintinadam@live.com>
2015-06-27 21:11:27 -0600

    moar cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # before_action :set_controller, :if => :is_development
-
-  private
-
-  # def is_development
-  #   Rails.env.development?
-  # end
-
-  # def set_controller
-  #   console
-  # end
 end
