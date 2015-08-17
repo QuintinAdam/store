2d84ea2090d5ded027b6dab6dd58bf42cf2887db
katherine <muedasnka@gmail.com>
2015-08-17 17:16:00 -0600

    adds phone number to header

--- app/overrides/pages_in_header.rb
+++ app/overrides/pages_in_header.rb
@@ -3,3 +3,14 @@ Deface::Override.new(virtual_path: "spree/shared/_main_nav_bar",
                      insert_bottom: "#main-nav-bar > ul:first-child",
                      partial: "spree/static_content/static_content_header",
                      disabled: false)
+
+Deface::Override.new(virtual_path: "spree/shared/_header",
+                     name: "add phone to header",
+                     insert_after: 'figure#logo',
+                     text:'
+                     <div class="header_phone" > 
+                      Phone: (407)443-1720
+                     </div> 
+
+                     ')
+                     
